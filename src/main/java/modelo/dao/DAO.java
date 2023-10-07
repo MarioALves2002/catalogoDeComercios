af9b2838f51e55ce374042deaa329791a10a7e81
MárioAlves <marioalves5627@gmail.com>
2023-10-06 23:54:58 -0300

    funcionando tudo

--- src/main/java/modelo/dao/DAO.java
+++ src/main/java/modelo/dao/DAO.java
@@ -8,8 +8,8 @@ public class DAO {
     public Connection conectar() throws Exception {
         Class.forName("com.mysql.jdbc.Driver");
         return DriverManager.getConnection(
-                "jdbc:mysql://192.168.24.139:3306/catalogo_comercios", 
-                "root", "123456789");
+                "jdbc:mysql:// localhost:3306/catalogo_comercios", 
+                "root", "root");
     }
 
 }
